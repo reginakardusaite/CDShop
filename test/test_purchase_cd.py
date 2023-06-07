96ec930bc20a073ac3d6e9a32af2eeec0c03ecbc
RK <regina.kardusaite@7digital.com>
2023-06-07 12:11:46 +0100

    test correct value submitted to charts

--- test/test_purchase_cd.py
+++ test/test_purchase_cd.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import Mock
 
 from src.cd import CD
 
@@ -6,6 +7,10 @@ class FakeCreditCard:
 
     def pay(self):
         return True
+
+class Chart:
+    def submit(self,cd):
+        pass
 
 
 class TestPurchaseCD(unittest.TestCase):
@@ -30,6 +35,15 @@ class TestPurchaseCD(unittest.TestCase):
         credit_card.pay = lambda: False
         cd.purchase(5, credit_card)
         self.assertEqual(cd.stock, 10)
+
+    def test_notify_chart_when_purchase_with_stock_and_valid_card(self):
+        cd = CD(20)
+        credit_card = FakeCreditCard()
+        credit_card.pay = lambda: True
+        chart = Chart()
+        chart.submit = Mock(return_value=True)
+        cd.purchase(12,credit_card)
+        chart.submit.assert_called_with(12)
 
 if __name__ == '__main__':
     unittest.main()
